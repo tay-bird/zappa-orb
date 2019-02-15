c7e20561698331895e812222d4f3fcf64e197181
Tay Frost <tay@circleci.com>
2019-02-14 18:53:31 -0800

    Add a basic set of tests
    This commit adds a set of test manifests and Python unittest cases.
    During the CI run, the manifests are processed with the latest dev
    release of the orb and the test cases are run. The test cases inspect
    the data structure to ensure the expected keys are present.

---
+++ test/test_executor.py
@@ -0,0 +1,20 @@
+from pprint import pprint as pp
+import os
+import unittest
+
+import yaml
+
+
+class TestExecutor(unittest.TestCase):
+
+    def setUp(self):
+        with open("executor.yml") as f:
+            self.config = yaml.load(f)
+
+    def test_job_uses_py37_image(self):
+        actual_images = [
+            docker_config['image']
+            for docker_config in self.config['jobs']['zappa/zappa-deploy']['docker']]
+        expected_images = ['circleci/python:3.7']
+
+        self.assertTrue(set(expected_images) == set(actual_images))
