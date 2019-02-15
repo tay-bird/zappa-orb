8346b30c8d24e2a31fa7e4053a568def6893b21b
Tay Frost <tay@circleci.com>
2019-02-15 15:00:53 -0800

    Add a second test for the all_stages: true case

--- test/test_all_stages.py
+++ test/test_all_stages.py
@@ -27,3 +27,35 @@ class TestAllStages(unittest.TestCase):
             'fi')
 
         self.assertTrue(expected_step == actual_step)
+
+    def test_all_stages_true_deploys_to_one_stage(self):
+        job = self.config['jobs']['zappa-deploy-all_stages-true']
+        actual_step = job['steps'][2]['run']['command']
+        pp(actual_step)
+        expected_step = (
+            'set +e\n'
+            'STATUS=$(pipenv run zappa status --all -j 2>&1 >/dev/null)\n'
+            'ALL_DEPLOYMENTS=$(cat zappa_settings.json | jq -r \'. | keys | join(" ")\')\n'
+            "NEW_DEPLOYMENTS=$(echo $STATUS | awk '{\n"
+            "  if ( $0 '\\!'~ /^Error: No Lambda.*deployed/ ) {\n"
+            '    exit 1\n'
+            '  }\n'
+            '  else print $4\n'
+            "}')\n"
+            'STATUS_EXIT_CODE=$?\n'
+            'NEW_DEPLOYMENT_COUNT=$(echo $NEW_DEPLOYMENTS | wc -w)\n'
+            'TOTAL_DEPLOYMENT_COUNT=$(echo $ALL_DEPLOYMENTS | wc -w)\n'
+            'set -e\n'
+            'if [[ "$STATUS_EXIT_CODE" != "0" ]] then\n'
+            '  echo "$STATUS\\nUnknown error!" && exit 1\n'
+            'elif [ "$NEW_DEPLOYMENT_COUNT" == "$TOTAL_DEPLOYMENT_COUNT" ]\n'
+            '  then pipenv run zappa deploy --all\n'
+            'elif [ "$NEW_DEPLOYMENT_COUNT" -gt "0" ]\n'
+            '  for DEPLOYMENT_NAME in $NEW_DEPLOYMENTS do\n'
+            "    STAGE=$(echo $DEPLOYMENT_NAME | awk -F- '{print $NF}')\n"
+            '    pipenv run zappa deploy $STAGE\n'
+            '  done\n'
+            'fi\n'
+            'pipenv run zappa update --all')
+
+        self.assertTrue(expected_step == actual_step)
